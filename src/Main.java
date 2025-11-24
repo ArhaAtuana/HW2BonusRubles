ed7f05d4ee2817174349dc0d10fa1e8b0f43030c
Ksenia Krestsova <arhaatuana@gmail.com>
2025-11-24 09:45:04 +0200

    reformate code

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,12 @@ public class Main {
     public static void main(String[] args) {
 
         int amount = 300;
-        int adding = 2600;
+        int adding = 60;
         int total = amount + adding;
 
-        if (adding > 1000){total = total + adding/100;}
+        if (adding > 1000) {
+            total = total + adding / 100;
+        }
         System.out.println(total);
     }
 }
