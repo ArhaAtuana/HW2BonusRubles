e65c6cafc47ef9ef16a3348e566cdfd785f2b4fb
Ksenia Krestsova <arhaatuana@gmail.com>
2025-11-24 12:55:37 +0200

    logic fixed

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,17 @@ public class Main {
         int amount = 300;
         int adding = 3_600;
         int total = amount + adding;
-
-        if (adding > 1000) {
-            total = total + adding / 100;
-        } else {
-            total = amount + adding;
-        }
-        System.out.println("Итоговый счет:" + total);
-
         int bonus;
 
         if (adding > 1000) {
-            bonus = adding/100;
+            bonus = adding / 100;
         } else {
             bonus = 0;
         }
+
+        total = total + bonus;
+
+        System.out.println("Итоговый счет:" + total);
         System.out.println("Бонус составил:" + bonus);
     }
 }
