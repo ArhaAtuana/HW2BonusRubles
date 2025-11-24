f122f6affe884b14016969f0ae048e0aad2f09f3
Ksenia Krestsova <arhaatuana@gmail.com>
2025-11-24 11:14:15 +0200

    bonus added

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,23 @@ public class Main {
     public static void main(String[] args) {
 
         int amount = 300;
-        int adding = 60;
+        int adding = 3_600;
         int total = amount + adding;
 
         if (adding > 1000) {
             total = total + adding / 100;
+        } else {
+            total = amount + adding;
         }
-        System.out.println(total);
+        System.out.println("Итоговый счет:" + total);
+
+        int bonus;
+
+        if (adding > 1000) {
+            bonus = adding/100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println("Бонус составил:" + bonus);
     }
 }
